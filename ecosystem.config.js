552a13782f9717869c7f27773475c85cf3235787
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-27 17:05:48 -0600

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   apps: [{
     name: 'Shared Portfolio',
-    script: './server.js',
-    interpreter: '/usr/bin/node',
-    args: '',
-    watch: true
+    script: 'server.js',
+    interpreter: '/usr/bin/authbind',
+    args: '--deep node'
   }]
 };
