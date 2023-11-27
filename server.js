6d7c8fff2caa3a0f6fb0dec016112409f1a20ece
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-26 22:13:34 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files (like CSS, JS, images) from a directory named 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the root URL ('/')
+app.get('/', (req, res) => {
+    // Send an HTML file or a response for the root URL
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    // Or res.send('Hello, World!');
+});
+
+const PORT = 58543;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
