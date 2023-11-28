a6c21fe2bfc80255cc0010a656e122bbc9c2a7af
willohrobbins <willoh_robbins@outlook.com>
2023-11-27 23:51:28 -0600

    cursor foolery

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
 document.getElementById('butt').addEventListener('click', function() {
     document.body.style.cursor = 'url(skull.png), auto';
 });
+
+document.body.style.cursor = 'url(cursor.png), auto';
