59dc5e2b33f6cdd7d404d30fa54ce9b0e97f6f91
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2023-11-27 23:16:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Check the host header to respond to 'markoviandevelopments.com' only
+app.use((req, res, next) => {
+    if (req.hostname === 'markoviandevelopments.com') {
+        next();
+    } else {
+        res.status(404).send('Not Found');
+    }
+});
+
 // Define a route for the root URL ('/')
 app.get('/', (req, res) => {
+    // Send the 'index.html' file for 'markoviandevelopments.com'
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Listen on port 80 for HTTP
 const PORT = 80;
 app.listen(PORT, () => {
-    console.log(`HTTP Server running on port ${PORT}`);
+    console.log(`Server running for markoviandevelopments.com on port ${PORT}`);
 });
